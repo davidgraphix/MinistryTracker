1ac74c268f80b6507b536423d5cb9dcf8dcf23a3
David smart <112566573+davidgraphix@users.noreply.github.com>
2025-04-01 17:12:30 +0100

    Update App.tsx

--- src/Types/App.tsx
+++ src/Types/App.tsx
@@ -1,5 +1,11 @@
 // src/App.tsx
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import { useState } from "react";
 import Dashboard from "../Dashboards/Dashboard";
 import AdminDashboard from "../Dashboards/AdminDashboard";
 import AttendancePage from "../pages/AttendancePage";
@@ -9,18 +15,30 @@ import Layout from "../components/Routes/Layout";
 import LoginPage from "../Login/LoginPage";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <AppProvider>
       <Router>
-        <Layout>
+        {isLoggedIn ? (
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/workers" element={<AdminDashboard />} />
+              <Route path="/attendance" element={<AttendancePage />} />
+              <Route path="/departments" element={<DepartmentsPage />} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </Layout>
+        ) : (
           <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/workers" element={<AdminDashboard />} />
-            <Route path="/attendance" element={<AttendancePage />} />
-            <Route path="/departments" element={<DepartmentsPage />} />
-            <Route path="/login" element={<LoginPage />} />
+            <Route
+              path="/login"
+              element={<LoginPage onLogin={() => setIsLoggedIn(true)} />}
+            />
+            <Route path="*" element={<Navigate to="/login" />} />
           </Routes>
-        </Layout>
+        )}
       </Router>
     </AppProvider>
   );
